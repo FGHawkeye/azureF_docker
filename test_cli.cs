5c47df0f347b678ae25dacb7b0d4a99cbaca50cf
Gabriel Ajaya <ajaya_gabriel@hotmail.com>
2019-06-20 18:49:48 -0300

    calculator added, dockerignore updated to exclude the console app publishing

--- test_cli.cs
+++ test_cli.cs
@@ -1,33 +1,73 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace AzureCLI
 {
     public static class test_cli
     {
-        [FunctionName("test_cli")]
+        [FunctionName("Calculate")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            //Find the numbers with regular exp in the recieved body
+            var numbers = Regex.Matches(requestBody, "[0-9]+").Cast<Match>().Select(m => m.Value).ToList();
+            //Find the operators with regular exp in the recieved body
+            var operators = Regex.Matches(requestBody, @"[+-\/*]").Cast<Match>().Select(m => m.Value).ToList();
+
+            //Casting list to the correct data type
+            var numbersToBeCalculated = numbers.ConvertAll(x => int.Parse(x));
+
+            //Checking if math operators are equal or more than the numbers to be calculated
+            if (operators.Count >= numbersToBeCalculated.Count)
+            {
+                return new BadRequestObjectResult("Wrong entry. Try again using one or more operations");
+            }
+
+            double result = numbersToBeCalculated.FirstOrDefault();
+
+            var j = 0;
+            foreach(int number in numbersToBeCalculated.Skip(1))
+            {
+                switch (operators[j])
+                {
+                    case "+":
+                        {
+                            result += number;
+                            break;
+                        }
+                    case "-":
+                        {
+                            result -= number;
+                            break;
+                        }
+                    case "*":
+                        {
+                            result *= number;
+                            break;
+                        }
+                    case "/":
+                        {
+                            result /= number;
+                            break;
+                        }
+                    default:
+                        break;
+                }
+                j++;
+            }
+            return (ActionResult)new OkObjectResult($"Total: {result}");
         }
     }
 }
